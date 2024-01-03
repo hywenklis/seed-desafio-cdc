bd3f8a6d642a05f1a5e26cbec83eff51f58fb85b
Hywenklis <hywenklis@hotmail.com>
2024-01-03 17:11:05 -0300

    Adiciona spotbugs

--- src/main/java/com/jornada/dev/eficiente/desafio1/web/advice/error/ErrorResponse.java
+++ src/main/java/com/jornada/dev/eficiente/desafio1/web/advice/error/ErrorResponse.java
@@ -2,4 +2,8 @@ package com.jornada.dev.eficiente.desafio1.web.advice.error;
 
 import java.util.List;
 
-public record ErrorResponse(List<ErrorDetails> errors) { }
+public record ErrorResponse(List<ErrorDetails> errors) {
+    public ErrorResponse {
+        errors = errors != null ? List.copyOf(errors) : List.of();
+    }
+}
